72f11c248006765f5751f033e0eecccb62b861ed
mustafasan088706 <san.mustafa0887@gmail.com>
2021-04-10 19:48:52 +0200

    made up all abstract classes for each value

--- src/Database/MyDatabase.java
+++ src/Database/MyDatabase.java
@@ -1,5 +1,25 @@
 package Database;
 
-public class MyDatabase {
+public abstract class MyDatabase {
 
+public MyDatabase() {
+	
+}
+
+	public abstract boolean connect (String sqliteDb);
+	public abstract boolean connect(String dbName,String userName,String userPass,String host,int port);
+	
+	public abstract void disConnected();
+	public abstract int addData(String insertQuery);
+	public abstract int addData (String nameOftable,String columns,String values);
+	public abstract ResultSet listOfvalues(String selectQuery);
+	
+	public abstract boolean deleteData(String query);
+	public abstract int deleteData(String nameOftable,String whereCondition);
+	public abstract int updateDatad(String query);
+	public abstract int updateData(String nameOftable,String update,String condition);
+	public abstract int totalNumberOfrows(String query);
+	public abstract int totalNumberOftables();
+	
+	
 }
