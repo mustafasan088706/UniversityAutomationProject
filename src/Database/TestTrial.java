35cdd74427fb7d60d1617640d3e82310a8e04937
mustafasan088706 <san.mustafa0887@gmail.com>
2021-04-11 19:32:32 +0200

    updated

--- src/Database/TestTrial.java
+++ src/Database/TestTrial.java
@@ -10,11 +10,20 @@ public class TestTrial {
 		
 		if (test.connect("testuniversity", "root", "" ,"localhost" , 3306)) {
 			
-			System.out.println("Connected To The Database...");
+			System.out.println("Connected To Database...");
+			
+			System.out.println("     SELECT ACTION :    " );
+			System.out.println(" 1- List Undergraduate Students :" );
+			System.out.println(" 2- Add New Student : " );
+			System.out.println(" 3- Delete A Student : " );
+			System.out.println(" 4- Update A Student : ");
+			
 		}else {
 			
 			System.out.println("Not Connected");
 		}
+
+		
 
 	}
 
