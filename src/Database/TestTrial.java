51c02725971cb1b057f8af8efa25610d485ac582
mustafasan088706 <san.mustafa0887@gmail.com>
2021-04-11 22:33:13 +0200

    edit all uncorrect codes

--- src/Database/TestTrial.java
+++ src/Database/TestTrial.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 import Database.MySqlDatabase;
 
 public class TestTrial {
-
+	
 	public static void main(String[] args) {
 	
 		MySqlDatabase test=new MySqlDatabase();
 		Scanner scanner =new Scanner(System.in);
-		
 		System.out.println("Please enter your username : ");
 		String userName=scanner.next();
 		System.out.println("Please enter your password :");
 		String password=scanner.next();
-		if(userName.equals("Admin")) {
+		
+		if("Admin".equalsIgnoreCase(userName)) {
 			
-			if(password.equals("1234")) {
+			if("1234".equals(password)) {
 				
-				 test.connect("testuniversity", "root", "" ,"localhost" , 330 );
+				if(test.connect("testuniversity", "root", "" ,"localhost" , 3306 )){
+					System.out.println("Connected To Database...");		
 					
-					System.out.println("Connected To Database...");
+				
+						
+				while(true) {
 					
 					System.out.println("     SELECT ACTION :    " );
 					System.out.println(" 1- List Undergraduate Students :" );
@@ -31,37 +34,47 @@ public class TestTrial {
 					
 					int select=scanner.nextInt();
 					
-					switch (select) {
-					case 1:
-						
-							break;
-						case 2:
-						
-								break;
-							case 3:
-						
-									break;
-								case 4:
-						
-										break;
 
-					default:
-						break;
+					if(select==1) {
+						
+						
+					}else if(select==2) {
+						
+						
+					}else if(select==3) {
+						
+						
+					}else if(select==4){
+						
+						
+					}else
+					{
+						System.out.println("Not Valid Selection ! ");
+						
 					}
+					
+					System.out.println("Either don't you want to go on to writing (break)  or you can push by any character! ");
+					String value=scanner.next();
+					if("break".equalsIgnoreCase(value)) {
+						break;
+						
+					}
+				}
 				
-			
+					
+					}else {
+						System.out.println("Not Connected !");
+					}
+					
 			}
-
-				
-			}
+		
+		}else {
 			
-	
-					else {
-			
-			System.out.println("Not Connected!");
-			System.out.println("Password or Username is not Correct!");
+			System.out.println("NOT VALID ! ");
 		}
-
+		
+		
+		
 	}
 
 }
