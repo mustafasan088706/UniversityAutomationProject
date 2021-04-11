907bd4322f483b574037c4fa9da2dd8a21fb7961
mustafasan088706 <san.mustafa0887@gmail.com>
2021-04-11 21:22:31 +0200

    updated

--- src/Database/TestTrial.java
+++ src/Database/TestTrial.java
@@ -1,5 +1,7 @@
 package Database;
 
+import java.util.Scanner;
+
 import Database.MySqlDatabase;
 
 public class TestTrial {
@@ -7,23 +9,58 @@ public class TestTrial {
 	public static void main(String[] args) {
 	
 		MySqlDatabase test=new MySqlDatabase();
+		Scanner scanner =new Scanner(System.in);
 		
-		if (test.connect("testuniversity", "root", "" ,"localhost" , 3306)) {
+		System.out.println("Please enter your username : ");
+		String userName=scanner.next();
+		System.out.println("Please enter your password :");
+		String password=scanner.next();
+		if(userName.equals("Admin")) {
 			
-			System.out.println("Connected To Database...");
-			
-			System.out.println("     SELECT ACTION :    " );
-			System.out.println(" 1- List Undergraduate Students :" );
-			System.out.println(" 2- Add New Student : " );
-			System.out.println(" 3- Delete A Student : " );
-			System.out.println(" 4- Update A Student : ");
-			
-		}else {
-			
-			System.out.println("Not Connected");
-		}
+			if(password.equals("1234")) {
+				
+				 test.connect("testuniversity", "root", "" ,"localhost" , 330 );
+					
+					System.out.println("Connected To Database...");
+					
+					System.out.println("     SELECT ACTION :    " );
+					System.out.println(" 1- List Undergraduate Students :" );
+					System.out.println(" 2- Add New Student : " );
+					System.out.println(" 3- Delete A Student : " );
+					System.out.println(" 4- Update A Student : ");
+					
+					int select=scanner.nextInt();
+					
+					switch (select) {
+					case 1:
+						
+							break;
+						case 2:
+						
+								break;
+							case 3:
+						
+									break;
+								case 4:
+						
+										break;
 
-		
+					default:
+						break;
+					}
+				
+			
+			}
+
+				
+			}
+			
+	
+					else {
+			
+			System.out.println("Not Connected!");
+			System.out.println("Password or Username is not Correct!");
+		}
 
 	}
 
